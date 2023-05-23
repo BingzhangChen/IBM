848d3d3737f8dd0574fdf5b41dbc6e5f75bb9024
Bingzhang Chen <bingzhang.chen@strath.ac.uk>
2023-05-23 19:11:34 +0100

    Update running file

--- Run/IBM.sh
+++ Run/IBM.sh
@@ -11,19 +11,19 @@
 #SBATCH --export=ALL
 #
 # Run in the standard partition (queue)
-#SBATCH --partition=dev
+#SBATCH --partition=standard
 #
 # Specify project account
 #SBATCH --account=chen-pdefio
 #
 # No. of tasks required
-#SBATCH --ntasks=2
+#SBATCH --ntasks=5 --nodes=1
 #
 # Distribute processes in round-robin fashion for load balancing
 #SBATCH --distribution=cyclic
 #
 # Specify (hard) runtime (HH:MM:SS)
-#SBATCH --time=0:30:00
+#SBATCH --time=100:00:00
 #
 # Job name
 #SBATCH --job-name=IBM
@@ -34,8 +34,6 @@
 
 module purge
 module load fftw/intel-2020.4/3.3.10
-module load netcdf-fortran/intel-2018.2/4.4.4
-module load netcdf/intel-2018.2/4.6.1
 
 #export LD_LIBRARY_PATH=/opt/software/netcdf/intel-2018.2/4.6.1/lib:$LD_LIBRARY_PATH
 #======================================================
